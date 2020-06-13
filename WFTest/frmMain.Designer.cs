fd80549b39fae666a0fe02832361cd445bc4cdf6
Randy <hunsra@comcast.net>
2020-06-13 08:17:34 -0700

    FIrst revision

---
+++ WFTest/frmMain.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WFTest
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFill = new System.Windows.Forms.Button();
+            this.dgFiles = new System.Windows.Forms.DataGridView();
+            this.cName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblFillInfo = new System.Windows.Forms.Label();
+            this.cbDisable = new System.Windows.Forms.CheckBox();
+            this.lblResizeInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgFiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFill
+            // 
+            this.btnFill.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnFill.Location = new System.Drawing.Point(22, 1040);
+            this.btnFill.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFill.Name = "btnFill";
+            this.btnFill.Size = new System.Drawing.Size(220, 75);
+            this.btnFill.TabIndex = 0;
+            this.btnFill.Text = "&Fill";
+            this.btnFill.UseVisualStyleBackColor = true;
+            this.btnFill.Click += new System.EventHandler(this.btnFill_Click);
+            // 
+            // dgFiles
+            // 
+            this.dgFiles.AllowUserToAddRows = false;
+            this.dgFiles.AllowUserToDeleteRows = false;
+            this.dgFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgFiles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.cName,
+            this.cDate});
+            this.dgFiles.Location = new System.Drawing.Point(22, 29);
+            this.dgFiles.Margin = new System.Windows.Forms.Padding(4);
+            this.dgFiles.Name = "dgFiles";
+            this.dgFiles.ReadOnly = true;
+            this.dgFiles.RowHeadersVisible = false;
+            this.dgFiles.RowHeadersWidth = 4;
+            this.dgFiles.RowTemplate.Height = 33;
+            this.dgFiles.Size = new System.Drawing.Size(964, 992);
+            this.dgFiles.TabIndex = 1;
+            // 
+            // cName
+            // 
+            this.cName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.cName.HeaderText = "Name";
+            this.cName.MinimumWidth = 10;
+            this.cName.Name = "cName";
+            this.cName.ReadOnly = true;
+            this.cName.Width = 859;
+            // 
+            // cDate
+            // 
+            this.cDate.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.cDate.HeaderText = "Date";
+            this.cDate.MinimumWidth = 10;
+            this.cDate.Name = "cDate";
+            this.cDate.ReadOnly = true;
+            this.cDate.Width = 102;
+            // 
+            // lblFillInfo
+            // 
+            this.lblFillInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblFillInfo.AutoSize = true;
+            this.lblFillInfo.Location = new System.Drawing.Point(264, 1049);
+            this.lblFillInfo.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblFillInfo.Name = "lblFillInfo";
+            this.lblFillInfo.Size = new System.Drawing.Size(0, 25);
+            this.lblFillInfo.TabIndex = 2;
+            // 
+            // cbDisable
+            // 
+            this.cbDisable.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbDisable.AutoSize = true;
+            this.cbDisable.Location = new System.Drawing.Point(714, 1067);
+            this.cbDisable.Name = "cbDisable";
+            this.cbDisable.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.cbDisable.Size = new System.Drawing.Size(271, 29);
+            this.cbDisable.TabIndex = 3;
+            this.cbDisable.Text = "Disable During Updates";
+            this.cbDisable.UseVisualStyleBackColor = true;
+            this.cbDisable.CheckedChanged += new System.EventHandler(this.cbDisable_CheckedChanged);
+            // 
+            // lblResizeInfo
+            // 
+            this.lblResizeInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblResizeInfo.AutoSize = true;
+            this.lblResizeInfo.Location = new System.Drawing.Point(264, 1090);
+            this.lblResizeInfo.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblResizeInfo.Name = "lblResizeInfo";
+            this.lblResizeInfo.Size = new System.Drawing.Size(0, 25);
+            this.lblResizeInfo.TabIndex = 4;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 1137);
+            this.Controls.Add(this.cbDisable);
+            this.Controls.Add(this.lblFillInfo);
+            this.Controls.Add(this.lblResizeInfo);
+            this.Controls.Add(this.dgFiles);
+            this.Controls.Add(this.btnFill);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmMain";
+            this.Text = "DataGridView Test";
+            ((System.ComponentModel.ISupportInitialize)(this.dgFiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFill;
+        private System.Windows.Forms.DataGridView dgFiles;
+        private System.Windows.Forms.Label lblFillInfo;
+        private System.Windows.Forms.CheckBox cbDisable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cDate;
+        private System.Windows.Forms.Label lblResizeInfo;
+    }
+}
---
+++ WFTest/frmMain.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFTest
+{
+    public partial class frmMain : Form
+    {
+        private bool _disabledDuringUpdates = false;
+        private Random _RNG = new Random((int)DateTime.Now.Ticks);
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            // Set controls to default state
+            dgFiles.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+            dgFiles.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+            cbDisable.Checked = _disabledDuringUpdates;
+            
+            // Add a handler for important events
+            Resize += FrmMain_Resize;
+        }
+
+        private void FrmMain_Resize(object sender, EventArgs e)
+        {
+            ResizeColumns();
+        }
+
+        private void ResizeColumns()
+        {
+            bool reenable = false;
+
+            // For timing
+            DateTime start = DateTime.Now;
+
+            if (dgFiles.Enabled == true && _disabledDuringUpdates == true)
+            {
+                dgFiles.Enabled = false;
+            }
+
+            dgFiles.AutoResizeColumn(0, DataGridViewAutoSizeColumnMode.AllCells);
+            dgFiles.AutoResizeColumn(1, DataGridViewAutoSizeColumnMode.AllCells);
+
+            // Calculate the width of the first column - all remaining width or width of widest text, whichever is larger
+            var current = dgFiles.Columns[0].Width;
+            int offset = (dgFiles.ScrollBars & ScrollBars.Vertical) != ScrollBars.None ? System.Windows.Forms.SystemInformation.VerticalScrollBarWidth : 0;
+            dgFiles.Columns[0].Width = Math.Max(current, dgFiles.Width - (SystemInformation.SizingBorderWidth * 2) - dgFiles.Columns[1].Width - (dgFiles.Columns[0].DividerWidth == 0 ? 1 : dgFiles.Columns[0].Width) - offset);
+
+            if (reenable == true)
+            {
+                dgFiles.Enabled = true;
+            }
+
+            // For timing
+            TimeSpan elapsed = DateTime.Now - start;
+            lblResizeInfo.Text = $"Resize time: {elapsed.TotalMilliseconds}ms";
+        }
+
+        private void btnFill_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+
+            // This is to maximize the work the datagridview needs to do to load the data
+            var gen = GC.GetGeneration(dgFiles);
+            dgFiles.Rows.Clear();
+            GC.Collect(gen, GCCollectionMode.Forced, true, true);
+            GC.WaitForFullGCComplete();
+
+            // For timing
+            DateTime start = DateTime.Now;
+            
+            if (_disabledDuringUpdates == true)
+            {
+                // Disable the entire control - this prevents visual updates
+                dgFiles.Enabled = false;
+            }
+            
+            // Add 1500 items to the data grid view
+            for (int i = 0; i < 1500; i++)
+            {
+                // Randomly set bolded font on about 30% of the rows
+                DataGridViewRow row = new DataGridViewRow();
+                var rn = _RNG.Next(1, 10);
+                if (rn >= 7)
+                {
+                    row.DefaultCellStyle = new DataGridViewCellStyle() { Font = new Font(DefaultFont, FontStyle.Bold) };
+                }
+                row.Cells.Add(new DataGridViewTextBoxCell());
+                row.Cells.Add(new DataGridViewTextBoxCell());
+                row.Cells[0].Value = $"Name {i:0000} - {rn}";
+                row.Cells[1].Value = $"{DateTime.Now:G} - {rn}";
+                dgFiles.Rows.Add(row);
+            }
+
+            // Force a resize
+            ResizeColumns();
+
+            if (_disabledDuringUpdates == true)
+            {
+                // Re-enable the entire control - this allows a final visual update
+                dgFiles.Enabled = true;
+            }
+            
+            // For timing
+            TimeSpan elapsed = DateTime.Now - start;
+            lblFillInfo.Text = $"Fill time: {elapsed.TotalMilliseconds}ms";
+
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void cbDisable_CheckedChanged(object sender, EventArgs e)
+        {
+            _disabledDuringUpdates = cbDisable.Checked;
+        }
+    }
+}
